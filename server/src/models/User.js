fdbbc3fb372844c5394424e423cb792f28c49dc9
unknown <deepakdube1102@gmail.com>
2026-06-01 20:17:02 +0530

    Update User model to explicitly query the 'users' collection

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -93,9 +93,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  
+
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -106,9 +106,9 @@ userSchema.pre('save', async function(next) {
 });
 
 // Compare password method
-userSchema.methods.comparePassword = async function(enteredPassword) {
+userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema, 'user');
+const User = mongoose.model('User', userSchema, 'users');
 export default User;
